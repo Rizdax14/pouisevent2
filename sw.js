722c5ffea18afd02dc412ae7cf364943dd520210
Rizdax14 <louismarcoux1404@gmail.com>
2026-05-17 13:30:14 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // VERSION: bump this string to force cache clear on all clients
-const CACHE_VERSION = 'pouis-v2-' + '20260513172302'2604152305';
+const CACHE_VERSION = 'pouis-v2-' + '20260517112827'2604152305';
 const ASSETS = [
   '/',
   '/index.html',
